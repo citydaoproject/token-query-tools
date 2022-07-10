6a309ef1d395b5f193675a1cacb103b54d41fc7b
Michael D. Norman <michael.norman@denormans.com>
2022-07-10 15:33:26 -0500

    refactor [#1]: extracted common moralis paging logic

--- src/services/moralis.ts
+++ src/services/moralis.ts
@@ -3,19 +3,42 @@ import Moralis from 'moralis/node';
 import { moralisApplicationId, moralisPageLimit, moralisRateLimitPerMinute, moralisServerUrl } from './config';
 import { NFTOwner } from './owners';
 
-export const fetchAllNFTOwners = async (address: string): Promise<NFTOwner[]> => {
+export const fetchAllNFTOwners = async (address: string): Promise<NFTOwner[]> =>
+  fetchAll((cursor) => fetchNFTOwners(address, cursor));
+
+const fetchNFTOwners = async (address: string, cursor?: string): Promise<MoralisResults<NFTOwner>> => {
+  const response = await Moralis.Web3API.token.getNFTOwners({ address, chain: 'eth', limit: moralisPageLimit, cursor });
+
+  return {
+    data: response.result?.map(({ token_id, owner_of }) => ({ nftId: token_id, ownerAddress: owner_of })) || [],
+    cursor: response.cursor,
+    page: response.page || 0,
+    pageSize: response.page_size || 0,
+    total: response.total || 0,
+  };
+};
+
+interface MoralisResults<T> {
+  data: T[];
+  cursor?: string;
+  page: number;
+  pageSize: number;
+  total: number;
+}
+
+const fetchAll = async <T, R extends MoralisResults<T>>(fetch: (cursor?: string) => Promise<R>): Promise<T[]> => {
   const limiter = new RateLimiter({ tokensPerInterval: moralisRateLimitPerMinute, interval: 'minute' });
 
   await Moralis.start({ serverUrl: moralisServerUrl, appId: moralisApplicationId });
 
-  const allResults: NFTOwner[] = [];
+  const allResults: T[] = [];
 
   let cursor: string | undefined = undefined;
   do {
     try {
       await limiter.removeTokens(1);
-      const partialResults = await fetchNFTOwners(address, cursor);
-      allResults.push(...partialResults.owners);
+      const partialResults = await fetch(cursor);
+      allResults.push(...partialResults.data);
 
       console.log(`Fetched page ${partialResults.page} of ${calcNumPages(partialResults)}`);
 
@@ -36,29 +59,6 @@ export const fetchAllNFTOwners = async (address: string): Promise<NFTOwner[]> =>
   return allResults;
 };
 
-interface MoralisResults {
-  cursor?: string;
-  page: number;
-  pageSize: number;
-  total: number;
-}
-
-interface FetchNFTOwnersResults extends MoralisResults {
-  owners: NFTOwner[];
-}
-
-const fetchNFTOwners = async (address: string, cursor?: string): Promise<FetchNFTOwnersResults> => {
-  const response = await Moralis.Web3API.token.getNFTOwners({ address, chain: 'eth', limit: moralisPageLimit, cursor });
-
-  return {
-    owners: response.result?.map(({ token_id, owner_of }) => ({ nftId: token_id, ownerAddress: owner_of })) || [],
-    cursor: response.cursor,
-    page: response.page || 0,
-    pageSize: response.page_size || 0,
-    total: response.total || 0,
-  };
-};
-
-const calcNumPages = (partialResults: FetchNFTOwnersResults) =>
+const calcNumPages = (partialResults: MoralisResults<any>) =>
   Math.floor(partialResults.total / partialResults.pageSize) +
   (partialResults.total % partialResults.pageSize != 0 ? 1 : 0);
